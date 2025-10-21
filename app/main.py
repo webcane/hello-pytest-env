2a0ddf7a212e182d27681691fbf735965e018548
Mikhail Niedre <mikhail.niedre@quantori.com>
2025-10-21 09:46:44 +0300

    delayed config creation

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
-from app.core.config import app_settings
+from app.core.config import get_app_settings
 import sys
 
 
+app_settings = get_app_settings()
+
 # validate that app_name is set and not empty
 if not app_settings.app_name or not app_settings.app_name.strip():
-    print("Ошибка: app_name не задан или пустой в конфиге.")
+    print("Error: 'app_name' not set in .env or is empty.")
     sys.exit(1)
 
 print(f"app_name: {app_settings.app_name}")
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,4 @@
 import pytest
-from app.core.config import reset_app_settings_cache
-
-
-# fixture to reset the cached app settings before each test
-# it garantees that changes to environment variables are reflected in app settings
-@pytest.fixture(autouse=True)
-def reset_app_settings_cache_fixture():
-    reset_app_settings_cache()
 
 
 # fixture for setting environment variables using monkeypatch
